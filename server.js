0ccb6fc2ccea959dfc9ef65b3dba04a7a285a360
Alex Crouch <acrouch06@gmail.com>
2020-08-06 17:41:55 -0500

    created server file

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+// const Workout = require("/models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+require("./routes/html-routes.js")(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethods", { useNewUrlParser: true });
+
+app.post("/submit", ({ body }, res) => {
+  
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
