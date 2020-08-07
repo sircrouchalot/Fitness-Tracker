02571a83e8a3360f07696ab547196abc78c697db
Alex Crouch <acrouch06@gmail.com>
2020-08-07 10:59:35 -0500

    last workout displays correctly now

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -22,18 +22,39 @@ module.exports = function (app) {
       });
   });
 
-  app.put("/api/exercises/:id", (req, res) => {
-    db.Workout.insert({})
-      .then((dbExercise) => {
-        res.json(dbExercise);
+  app.get("/api/workouts/:id", (req, res) => {
+    db.Workout.find({_id: req.params.id})
+      .then((dbWorkouts) => {
+        res.json(dbWorkouts);
       })
       .catch((err) => {
         res.json(err);
       });
   });
 
+  app.put("/api/workouts/:id", (req, res) => {
+    console.log(req.body);
+    db.Workout.findOneAndUpdate( {
+      _id: req.params.id
+    },
+    {
+      $push: {
+        exercises: req.body
+      }
+    }) 
+    .then((dbExercise) => {
+      res.json(dbExercise);
+    })
+    .catch((err) => {
+      res.json(err);
+     });
+  });
+
   app.post("/api/workouts", (req, res) => {
-    db.Workout.create({})
+    db.Workout.create({
+      day: new Date().setDate(new Date().getDate()),
+      exercises: []
+    })
       .then((dbWorkouts) => {
         res.json(dbWorkouts);
       })
